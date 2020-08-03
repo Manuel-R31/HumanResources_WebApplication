6073abf9acdc8eb7b9a057356586aa4b7cc3a425
Oscar Quintanilla <38816404+owqh@users.noreply.github.com>
2020-08-03 02:21:38 -0600

    Creacion de plantilla completa y preparacion para crud

---
+++ src/java/sv/bitlab/utilidades/UtilidadesManejador.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.bitlab.utilidades;
+
+import sv.bitlab.manejadores.SesionUsuario;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.FacesEvent;
+
+/**
+ *
+ * @author Oscar
+ */
+public class UtilidadesManejador {
+    
+    //Envia un mensaje personalizado
+    public static void lanzarMensaje(FacesMessage.Severity severidad, String encabezado, String detalle){
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severidad, encabezado, detalle));
+    }
+    
+    //Envia un mensaje de informacion
+    public static void lanzarInfo(String encabezado, String detalle){
+        lanzarMensaje(FacesMessage.SEVERITY_INFO, encabezado, detalle);
+    }
+    
+    //Envia un mensaje de error
+    public static void lanzarError(String encabezado, String detalle){
+        lanzarMensaje(FacesMessage.SEVERITY_ERROR, encabezado, detalle);
+    }
+    
+    //Envia un mensaje de error
+    public static void lanzarAdvertencia(String encabezado, String detalle){
+        lanzarMensaje(FacesMessage.SEVERITY_WARN, encabezado, detalle);
+    }
+    
+    public static void redireccion(String pagina){
+        ExternalContext contex = FacesContext.getCurrentInstance().getExternalContext();
+        try {
+            contex.redirect(contex.getRequestContextPath()+"/"+pagina+".dev");
+        } catch (IOException e) {
+            Logger.getLogger(SesionUsuario.class.getName());
+        }
+    }
+}
