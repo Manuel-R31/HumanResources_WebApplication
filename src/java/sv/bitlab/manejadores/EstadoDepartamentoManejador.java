a6469394931773cb679fe3feecd51b6e2b94f983
Manuel Ramos <67672732+Manuel-R31@users.noreply.github.com>
2020-08-07 07:19:30 -0600

    Completando Admin

--- src/java/sv/bitlab/manejadores/EstadoDepartamentoManejador.java
+++ src/java/sv/bitlab/manejadores/EstadoDepartamentoManejador.java
@@ -5,9 +5,12 @@
  */
 package sv.bitlab.manejadores;
 
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import sv.bitlab.controladores.EstadoDepartamentoControlador;
+import sv.bitlab.entidades.Departamento;
 import sv.bitlab.entidades.EstadoDepartamento;
 
 /**
@@ -18,7 +21,23 @@ import sv.bitlab.entidades.EstadoDepartamento;
 @ViewScoped
 public class EstadoDepartamentoManejador extends ManejadorAbstracto<EstadoDepartamento>{
 
-    private EstadoDepartamentoControlador estadoDepartamentoControlador;
+    private EstadoDepartamentoControlador estadoDepartamentoControlador = new EstadoDepartamentoControlador();
+    private List<EstadoDepartamento> estadoDepList;
+    private Departamento departamento = new Departamento();
+    private EstadoDepartamento estadoDepartamento = new EstadoDepartamento();
+    
+    @PostConstruct
+    public void CargarEstadosDep(){
+        estadoDepList = estadoDepartamentoControlador.encontrarEntidades();
+    }
+
+    public EstadoDepartamentoControlador getEstadoDepartamentoControlador() {
+        return estadoDepartamentoControlador;
+    }
+
+    public void setEstadoDepartamentoControlador(EstadoDepartamentoControlador estadoDepartamentoControlador) {
+        this.estadoDepartamentoControlador = estadoDepartamentoControlador;
+    }
     
     public EstadoDepartamentoManejador() {
         super(EstadoDepartamento.class);
@@ -28,6 +47,30 @@ public class EstadoDepartamentoManejador extends ManejadorAbstracto<EstadoDepart
     @Override
     public EstadoDepartamentoControlador getControlador() {
         return estadoDepartamentoControlador;
+    }
+
+    public List<EstadoDepartamento> getEstadoDepList() {
+        return estadoDepList;
+    }
+
+    public void setEstadoDepList(List<EstadoDepartamento> estadoDepList) {
+        this.estadoDepList = estadoDepList;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public EstadoDepartamento getEstadoDepartamento() {
+        return estadoDepartamento;
+    }
+
+    public void setEstadoDepartamento(EstadoDepartamento estadoDepartamento) {
+        this.estadoDepartamento = estadoDepartamento;
     }
     
     
